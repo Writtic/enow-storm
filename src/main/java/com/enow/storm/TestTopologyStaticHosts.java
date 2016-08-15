78033694e8795f56d9db94d151c76afc1ea1847f
writtic <writtic@gmail.com>
2016-08-15 16:45:18 +0900

    Edit TestTopologyStaticHosts.java
    Add org.apache.storm.StormSubmitter

--- src/main/java/com/enow/storm/TestTopologyStaticHosts.java
+++ src/main/java/com/enow/storm/TestTopologyStaticHosts.java
@@ -6,6 +6,7 @@ package com.enow.storm;
 
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
 import org.apache.storm.kafka.*;
 import org.apache.storm.spout.SchemeAsMultiScheme;
 import org.apache.storm.topology.BasicOutputCollector;
@@ -14,6 +15,8 @@ import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.topology.base.BaseBasicBolt;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.kafka.trident.GlobalPartitionInformation;
+
+import java.util.Arrays;
 
 public class TestTopologyStaticHosts {
 
@@ -41,10 +44,20 @@ public class TestTopologyStaticHosts {
         TopologyBuilder builder = new TopologyBuilder();
         builder.setSpout("words", new KafkaSpout(kafkaConfig), 10);
         builder.setBolt("print", new PrinterBolt()).shuffleGrouping("words");
-        LocalCluster cluster = new LocalCluster();
         Config config = new Config();
-        cluster.submitTopology("kafka-test", config, builder.createTopology());
-
+        if (args != null && args.length > 1) {
+            String name = args[1];
+            String dockerIp = args[2];
+            config.setNumWorkers(2);
+            config.setMaxTaskParallelism(5);
+            config.put(Config.NIMBUS_THRIFT_PORT, 6627);
+            config.put(Config.STORM_ZOOKEEPER_PORT, 2181);
+            config.put(Config.STORM_ZOOKEEPER_SERVERS, Arrays.asList(dockerIp));
+            StormSubmitter.submitTopology(name, config, builder.createTopology());
+        } else {
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("test", config, builder.createTopology());
+        }
         Thread.sleep(600000);
 
     }
